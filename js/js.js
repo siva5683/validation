545f4de1a1dacce1cb3b588c682a50418f697d51
siva5683 <mr.joker87654@gmail.com>
2025-09-01 09:29:34 +0530

    get

---
+++ js/js.js
@@ -0,0 +1,28 @@
+var userName = document.getElementById('username');
+var userNameMsg = document.getElementById('username-msg');
+var password = document.getElementById('password');
+var passwordMsg = document.getElementById('password-msg');
+userName.addEventListener('input', function (e) {
+    var regex = /^[A-Za-z0-9]{8}$/;  
+    var res = regex.test(e.target.value);
+
+    if (res) {
+        userNameMsg.textContent = "Valid Username";
+        userNameMsg.className = "valid";
+    } else {
+        userNameMsg.textContent = "Not a Valid Username";
+        userNameMsg.className = "invalid";
+    }
+});
+password.addEventListener('input', function (e) {
+    var regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&*])[A-Za-z\d@$%#&*!]{8,}$/;
+    var res = regex.test(e.target.value);
+
+    if (res) {
+        passwordMsg.textContent = "Valid Password ";
+        passwordMsg.className = "valid";
+    } else {
+        passwordMsg.textContent = "Password Not Valid";
+        passwordMsg.className = "invalid";
+    }
+});
